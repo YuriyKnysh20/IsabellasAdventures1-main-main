25e7313ac63067ecbaa8ef9c272db8d46a091e4a
Khasan000 <zkurycin@gmail.com>
2023-10-12 23:10:01 +0300

    yes

---
+++ Assets/Scripts/SwitchLevel/SwitchLevel.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SwitchLevel : MonoBehaviour
+{
+    [Header("Выбор типа ввода") ] [Tooltip("При выборе одного типа ввода - игнорируется другой")]
+    [SerializeField] private Chooseinput _inputSelection;
+
+    
+
+    
+    [Space(3)]
+    [Header("Поля для ввода")]
+    [SerializeField] private int _sceneIndex;
+    [SerializeField] private string _sceneName;
+    
+    private Button _button;
+
+    private void Start()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    public void Switch()
+    {
+        if (_inputSelection == Chooseinput.inputString)
+        {
+            SceneManager.LoadScene(_sceneName);
+            Debug.Log("string");
+        }
+
+        if (_inputSelection == Chooseinput.inputIndex)
+        {
+            SceneManager.LoadScene(_sceneIndex);
+            Debug.Log("Index");
+        }
+
+    }
+}
+
+public enum Chooseinput
+{
+    inputString,
+    inputIndex
+} 
