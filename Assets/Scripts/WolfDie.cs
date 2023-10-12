ec423fa7fe67629bc32f32bf415d9bca573a0f14
Khasan000 <zkurycin@gmail.com>
2023-10-12 14:47:28 +0300

    Revert "Merge branch 'main' of https://github.com/YuriyKnysh20/IsabellasAdventures1-main-main"
    This reverts commit b5be1f8e2211a124471b8665f1e2a6e66ad10592, reversing
    changes made to 4ff7d7cf65541d94d5168185dc30ebd9fd62052a.

---
+++ Assets/Scripts/WolfDie.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
+public class WolfDie : MonoBehaviour
+{
+    [SerializeField] private Wolf _wolf;
+    [SerializeField] private int _enemyHealth;
+    
+    public AudioClip[] din;
+    private int audioCount, audioId;
+    private bool _canHurt;
+    private int _maxHealth;
+    public int CurrenEnemyHealth => _enemyHealth;
+    public event UnityAction<int, int> EnemyHealthCheanged;
+
+    private void Start()
+    {
+        _maxHealth = _enemyHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        _enemyHealth -= amount;
+        StartCoroutine(Hurt());
+        EnemyHealthCheanged?.Invoke(_enemyHealth, _maxHealth);
+        if (_enemyHealth <= 0)
+        {
+            _wolf.ItemDrop();
+            _wolf.Experience();
+            Die();
+            //PlaySlashAudio();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator Hurt()
+    {
+        if (_enemyHealth > 0)
+        {
+            _canHurt = false;
+            yield return new WaitForSeconds(0.1f);
+            _canHurt = true;
+        }
+    }
+
+    private void PlaySlashAudio()
+    {
+        audioCount = din.Length;
+        audioId = Random.Range(0, audioCount);
+        gameObject.GetComponent<AudioSource>().PlayOneShot(din[audioId]);
+    }
+
+    // void OnTriggerEnter2D(Collider2D col)
+    // {
+    //     if (col.tag == "Player")
+    //     {
+    //         //Instantiate(bloodSpot, transform.position, Quaternion.identity);
+    //         PlaySlashAudio();
+    //     }
+    // }
+}
